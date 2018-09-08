69707381cc0ae4ff35f5b0f4f2d5a33ce5a602f9
Calvin Tantio <calvin.tantio@outlook.com>
2018-09-09 05:36:10 +0800

    Rectify error

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ function handleMessage(sender_psid, received_message) {
       case 0:
         db.collection('shares').add({
           isAvailable: true,
-          submissionTime: admin.firestore.Timestamp.fromDate(new Date())
+          submissionTime: admin.firestore.Timestamp.fromDate(new Date().toDateString('yyyy-MM-dd HH:mm:ss'))
         }).then(ref => {
           docId = ref.id;
         });
