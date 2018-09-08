0fb4ec25b411509b81b07e8e3a53f2e23f480178
Calvin Tantio <calvin.tantio@outlook.com>
2018-09-09 03:27:06 +0800

    Fixed messenger problem

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ let express = require('express'),
   bodyParser = require('body-parser'),
   app = express();
 
+const request = require('request');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
@@ -38,84 +40,106 @@ app.get('/webhook', (req, res) => {
   }
 });
 
-app.post('/webhook', (req, res) => {
+app.post('/webhook', (req, res) => {  
 
+  // Parse the request body from the POST
   let body = req.body;
 
+  // Check the webhook event is from a Page subscription
   if (body.object === 'page') {
 
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function (entry) {
+    // Iterate over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
 
-      // Gets the message. entry.messaging is an array, but
-      // will only ever contain one message, so we get index 0
+      // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
       console.log(webhook_event);
-
+    
+    
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
-
+    
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
       if (webhook_event.message) {
-        console.log(webhook_event.message)
-        handleMessage(sender_psid, webhook_event.message)
+        handleMessage(sender_psid, webhook_event.message);        
       } else if (webhook_event.postback) {
-        console.log(webhook_event.postback)
-        handlePostback(sender_psid, webhook_event.postback)
+        handlePostback(sender_psid, webhook_event.postback);
       }
+      
     });
 
-    // Returns a '200 OK' response to all requests
+    // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
+
   } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
+    // Return a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 
 });
 
-// Handles messages events
-const handleMessage = (sender_psid, received_message) => {
+function handleMessage(sender_psid, received_message) {
   let response;
-
-  if (received_message.text) {
-    res.send('Hello World!')
-  }
+  
+  // Checks if the message contains text
+  if (received_message.text) {    
+    // Create the payload for a basic text message, which
+    // will be added to the body of our request to the Send API
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+    }
+  } else if (received_message.attachments) {
+    // Get the URL of the message attachment
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
+            "image_url": attachment_url,
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Yes!",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "No!",
+                "payload": "no",
+              }
+            ],
+          }]
+        }
+      }
+    }
+  } 
+  
+  // Send the response message
+  callSendAPI(sender_psid, response);    
 }
 
 // 
 
-const handlePostback = (sender_psid, received_postback) => {
+function handlePostback(sender_psid, received_postback) {
   let response;
-
+  
   // Get the payload for the postback
   let payload = received_postback.payload;
 
-  if (payload === 'GET_STARTED') {
-    response = askTemplate('Are you a Cat or Dog Person?');
-    callSendAPI(sender_psid, response);
-    console.log('inside payload and printing response because of callSendAPI')
-  }
-
   // Set the response based on the postback payload
-  if (payload === 'CAT_PICS') {
-    response = imageTemplate('cats', sender_psid);
-    callSendAPI(sender_psid, response, function () {
-      callSendAPI(sender_psid, askTemplate('Show me more'));
-    });
-  } else if (payload === 'DOG_PICS') {
-    response = imageTemplate('dogs', sender_psid);
-    callSendAPI(sender_psid, response, function () {
-      callSendAPI(sender_psid, askTemplate('Show me more'));
-    });
-  } else if (payload) {
-    response = askTemplate('Are you a Cat or Dog Person?');
-    callSendAPI(sender_psid, response);
-    console.log('inside payload and printing response because of callSendAPI')
+  if (payload === 'yes') {
+    response = { "text": "Thanks!" }
+  } else if (payload === 'no') {
+    response = { "text": "Oops, try sending another image." }
   }
   // Send the message to acknowledge the postback
+  callSendAPI(sender_psid, response);
 }
 
 const callSendAPI = (sender_psid, response, cb = null) => {
@@ -128,7 +152,7 @@ const callSendAPI = (sender_psid, response, cb = null) => {
 
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": "EAAQZAHMkoHGIBAHkoddZCxT4G7FWB4aaaDY1jlFju8FBwPRNJn0ZA0hCAKtafqZBKkJ6rXTVwzzUFcPDeDhCxfNqTHXOGXQMOuXCsZAKrEVZBeR3YZCY4J58OMbV1bpSo8oZAllqBziliZBAhqZCG4lquOTuBlcU5cnXwKxJ0UiKqahW3yVUPO9Ffz" },
+    "qs": { "access_token": "EAAQZAHMkoHGIBAPWCt3PggojczuYUTljaxfK5lfGJJSzOQOHwBcuv67pCvUcNUi0P8NiZBEXpPUK6M2UjTZAVKbvqLYzt3Gt83ELZCi3eF5EWtMplDoa86aU8Cn1yHQXZAFxJqpeZAx29Vsb4eayfPRjz53xZBstlVLCTYnFOYgmZCbUFRZAoavcK" },
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
@@ -136,6 +160,7 @@ const callSendAPI = (sender_psid, response, cb = null) => {
       if (cb) {
         cb();
       }
+      console.log("message sent");
     } else {
       console.error("Unable to send message:" + err);
     }
