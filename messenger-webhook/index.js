8921a1028b1549e8b9bec247b6bfdfc0374a4734
Harsh Gadodia <harsh@u.nus.edu>
2018-09-08 22:39:58 +0800

    x

--- messenger-webhook/index.js
+++ messenger-webhook/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
  
 app.listen(process.env.PORT || 1337, () => console.log('Example app listening on port 8989!'));
  
-app.get('/', (req, res) => res.send('583615801'));
+//app.get('/', (req, res) => res.send('583615801'));
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
@@ -74,31 +74,3 @@ app.post('/webhook', (req, res) => {
  
 });
 
-
-// Accepts GET requests at the /webhook endpoint
-app.get('/webhook', (req, res) => {
-  
-  /** UPDATE YOUR VERIFY TOKEN **/
-  const VERIFY_TOKEN = "1234567890";
-  
-  // Parse params from the webhook verification request
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
-  // Check if a token and mode were sent
-  if (mode && token) {
-  
-    // Check the mode and token sent are correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
-      // Respond with 200 OK and challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
-    }
-  }
-});
